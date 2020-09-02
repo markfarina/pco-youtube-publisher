7f3decc461b03c0b47f72b9ff18fb96833d164bf
markfarina <49366555+markfarina@users.noreply.github.com>
2020-09-02 20:48:38 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,15 +2,15 @@
 #Change all the /home/user to /home/your_home_dir
 
 # Add to crontab run every 15 min
-# */15 * * * * /home/user/pco-youtube-publisher/update_publishing.sh > /home/user/pco-youtube-publisher/log.txt
+# */15 * * * * /home/mark/pco-youtube-publisher/update_publishing.sh > /home/mark/pco-youtube-publisher/log.txt
 
 
 timestamp() {
   date +"%T"
 }
-cd /home/user/pco-youtube-publisher
-source /home/user/pco-youtube-publisher/venv/bin/activate
-python -V >> /home/user/pco-youtube-publisher/log.txt
+cd /home/mark/pco-youtube-publisher
+source /home/mark/pco-youtube-publisher/venv/bin/activate
+python -V >> /home/mark/pco-youtube-publisher/log.txt
 echo "Running script"
-python3.8 /home/user/pco-youtube-publisher/pco-youtube-publisher.py >> log.txt
+python3.8 /home/mark/pco-youtube-publisher/pco-youtube-publisher.py >> log.txt
 echo "All done" $(timestamp)
